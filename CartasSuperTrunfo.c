e536ae6c6d67f6fc41b33590e9eb8f55eb4cbb22
pedrolkaas <151377553+pedrolkaas@users.noreply.github.com>
2025-02-25 23:33:59 +0000

    erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 
 
 int main() {
     
-    char estado;
-    char código_da_carta[50];
-    char Cidade[50];
-    int populaçao;
-    float área_em_km²;
-    float PIB;
-    int pontos_turísticos;
-    
-    printf("digite o estado: \n");
-    scanf("%d", &estado);
+     char estado;
+     char código_da_carta[50];
+     char Cidade[50];
+     int populaçao;
+     float área_em_km²;
+     float PIB;
+     int pontos_turísticos;
+     
+     printf("digite o estado: \n");
+     scanf("%d", &estado);
     
     printf("digite o código da carta:\n");
     scanf("%s", &código_da_carta);
     
-    printf("digite a cidade:\n");
-    scanf("%s", &cidade);
+     printf("digite a cidade:\n");
+     scanf("%s", &cidade);
     
-    printf("digite a populaçao:\n);
-    scanf("%d", &populaçao);
+     printf("digite a populaçao:\n);
+     scanf("%d", &populaçao);
     
     printf("digite a áre em km²:\n");
     scanf("%d", &Área_em_km²);
