9602e90dec1da0976b5bd21ea6348efd4ebdad97
pedrolkaas <151377553+pedrolkaas@users.noreply.github.com>
2025-02-26 19:14:34 +0000

    sem erross

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main() {
      char codigo_da_carta1[50], codigo_da_carta2[50];
      char Cidade1[50], Cidade2[50];
      int Populacao1, Populacao2;
-     float area_em_km1, area_em_kmA2;
+     float area_em_km1, area_em_km2;
      float PIB1, PIB2;
      int pontos_turisticos1, pontos_turisticos2;
      
@@ -32,6 +32,27 @@ int main() {
      printf("digite o numero de pontos turisticos da cidade doestado1:\n");
      scanf("%d", &pontos_turisticos1);
 
+     printf("digite o estado2: \n");
+     scanf("%c", &estado2);
+    
+     printf("digite o código da carta do estado2:\n");
+     scanf("%s", &codigo_da_carta2);
+    
+     printf("digite a cidade do estado2:\n");
+     scanf("%s", &Cidade2);
+    
+     printf("digite a populacao da cidade do estado2:\n");
+     scanf("%d", &Populacao2);
+    
+     printf("digite a are em km da cidade do estado2:\n");
+     scanf("%f", &area_em_km2);
+    
+     printf("digite o PIB da cidade do estado2:\n");
+     scanf("%f", &PIB2);
+    
+     printf("digite o numero de pontos turisticos da cidade doestado2:\n");
+     scanf("%d", &pontos_turisticos2);
+
      printf("estado 1\n");
     
      printf("estado: %c\n", estado1);
@@ -42,6 +63,16 @@ int main() {
      printf("PIB: %f\n", PIB1);
      printf("pontos turisticos: %d\n", pontos_turisticos1);
     
+
+     printf("estado 2\n");
+    
+     printf("estado: %c\n", estado2);
+     printf("codigo da carta: %s\n", codigo_da_carta2);
+     printf("Cidade: %s\n", Cidade2);
+     printf("Populacao: %d\n", Populacao2);
+     printf("are em km: %f\n", area_em_km2);
+     printf("PIB: %f\n", PIB2);
+     printf("pontos turisticos: %d\n", pontos_turisticos2);
     
     
      return 0;
